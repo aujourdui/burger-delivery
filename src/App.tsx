d468310d4abbfc3559678f20685fd55866c90a07
kyosuke ito <kyosuke.ito@gmail.com>
2022-02-08 11:16:08 -0800

    add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import "./App.css";
 import { FC } from "react";
-import { useSelector } from "./redux/store";
-import { useDispatch } from "react-redux";
-import { additional, subtraction } from "./redux/counterSlice";
+// import { useSelector } from "./redux/store";
+// import { useDispatch } from "react-redux";
+// import { additional, subtraction } from "./redux/counterSlice";
 
 const App: FC = () => {
-  const count = useSelector((state: any) => state.counter.count);
-  const dispatch = useDispatch();
+  // const count = useSelector((state: any) => state.counter.count);
+  // const dispatch = useDispatch();
   return (
     <div className="App">
-      <h1>Count: {count}</h1>
+      <div className="text-3xl font-bold underline">
+        <h1>Hello world!</h1>
+        {/* <h1>Count: {count}</h1>
       <button onClick={() => dispatch(additional(1))}>UP</button>
-      <button onClick={() => dispatch(subtraction(1))}>DOWN</button>
+      <button onClick={() => dispatch(subtraction(1))}>DOWN</button> */}
+      </div>
     </div>
   );
 };
