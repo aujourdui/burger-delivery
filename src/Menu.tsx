9b87bdbc69d06aadb1141f025fa5e3b8703127ef
kyosuke ito <kyosuke.ito@gmail.com>
2022-02-09 12:34:04 -0800

    add basic redux function to count order

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,15 +1,32 @@
 import { useState, useEffect, FC } from "react";
 import axios from "axios";
-
-// http://jsonblob.com/api/940706479976235008
+import { useSelector } from "./redux/store";
+import { useDispatch } from "react-redux";
+import { additional } from "./redux/counterSlice";
 
 const Menu: FC = () => {
   const [menus, setMenus] = useState([]);
+  const count = useSelector((state: any) => state.counter.count);
+  const dispatch = useDispatch();
+
   useEffect(() => {
     axios.get("http://jsonblob.com/api/940706479976235008").then((res) => {
       setMenus(res.data);
     });
   }, []);
+
+  // const addItem = (id) => {
+  //   window.confirm("Are you sure to add this item to cart?");
+  //   setItem((prevState) => {
+  //     return { ...prevState, id };
+  //   });
+  //   console.log(item);
+  // };
+
+  const addItem = () => {
+    window.confirm("Are you sure to add this item to cart?");
+    dispatch(additional(1));
+  };
 
   return (
     <>
@@ -18,10 +35,18 @@ const Menu: FC = () => {
           <h1>Our menu</h1>
         </div>
       </div>
-      <div className="grid gap-4 grid-cols-3 grid-rows-3 ">
+      <div className="grid gap-4 grid-cols-3 grid-rows-3">
         {menus.map((menu) => (
-          <div key={menu.id} className="text-3xl font-bold underline ml-8 mt-4">
+          <div
+            key={menu.id}
+            className="ml-8 mr-8 mt-8 hover:opacity-90 hover:cursor-pointer"
+            onClick={() => addItem()}
+          >
             <img src={menu.image} alt="menu" />
+            <p className="text-2xl">{menu.title}</p>
+            <p>price: {menu.price}</p>
+            <p>{menu.description}</p>
+            <p>{count}</p>
           </div>
         ))}
       </div>
