a9a62c338fac631eba3e09da96d427f94896bc36
kyosuke ito <kyosuke.ito@gmail.com>
2022-02-09 10:53:17 -0800

    add fetch data function by axios

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,9 +1,16 @@
-import { FC } from "react";
+import { useState, useEffect, FC } from "react";
 import axios from "axios";
 
 // http://jsonblob.com/api/940706479976235008
 
 const Menu: FC = () => {
+  const [menus, setMenus] = useState([]);
+  useEffect(() => {
+    axios.get("http://jsonblob.com/api/940706479976235008").then((res) => {
+      setMenus(res.data);
+    });
+  }, []);
+
   return (
     <>
       <div className="flex justify-center">
@@ -12,7 +19,16 @@ const Menu: FC = () => {
         </div>
       </div>
       <div className="flex justify-center">
-        <div className="text-3xl font-bold underline ml-8 mt-4">
+        {menus.map((menu) => (
+          <div key={menu.id} className="text-3xl font-bold underline ml-8 mt-4">
+            <img
+              // className="h-55"
+              src={menu.image}
+              alt="menu"
+            />
+          </div>
+        ))}
+        {/* <div className="text-3xl font-bold underline ml-8 mt-4">
           <img
             className="h-50"
             src="https://www.vmcdn.ca/f/files/via/images/pourhouse_burger_2_photochrisgiannakos.jpg;w=960"
@@ -52,7 +68,7 @@ const Menu: FC = () => {
             src="https://www.vmcdn.ca/f/files/via/images/pourhouse_burger_2_photochrisgiannakos.jpg;w=960"
             alt="menu"
           />
-        </div>
+        </div> */}
       </div>
     </>
   );
