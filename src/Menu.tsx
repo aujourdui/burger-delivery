e3ade127cba341746676e8ba6d6d755420a571b8
kyosuke ito <kyosuke.ito@gmail.com>
2022-02-09 11:17:48 -0800

    add different photos

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -18,57 +18,12 @@ const Menu: FC = () => {
           <h1>Our menu</h1>
         </div>
       </div>
-      <div className="flex justify-center">
+      <div className="grid gap-4 grid-cols-3 grid-rows-3 ">
         {menus.map((menu) => (
           <div key={menu.id} className="text-3xl font-bold underline ml-8 mt-4">
-            <img
-              // className="h-55"
-              src={menu.image}
-              alt="menu"
-            />
+            <img src={menu.image} alt="menu" />
           </div>
         ))}
-        {/* <div className="text-3xl font-bold underline ml-8 mt-4">
-          <img
-            className="h-50"
-            src="https://www.vmcdn.ca/f/files/via/images/pourhouse_burger_2_photochrisgiannakos.jpg;w=960"
-            alt="menu"
-          />
-        </div>
-        <div className="text-3xl font-bold underline ml-8 mt-4">
-          <img
-            className="h-55"
-            src="https://www.vmcdn.ca/f/files/via/images/mimis-burgers.jpg;w=960"
-            alt="menu"
-          />
-        </div>
-        <div className="text-3xl font-bold underline ml-8 mt-4">
-          <img
-            className="h-50"
-            src="https://www.vmcdn.ca/f/files/via/images/not-in-kansas-downlow-burgers-vancouver.jpg;w=960"
-            alt="menu"
-          />
-        </div>
-      </div>
-      <div className="flex justify-center">
-        <div className="text-3xl font-bold underline ml-8 mt-4">
-          <img
-            src="https://www.vmcdn.ca/f/files/via/images/pourhouse_burger_2_photochrisgiannakos.jpg;w=960"
-            alt="menu"
-          />
-        </div>
-        <div className="text-3xl font-bold underline ml-8 mt-4">
-          <img
-            src="https://www.vmcdn.ca/f/files/via/images/pourhouse_burger_2_photochrisgiannakos.jpg;w=960"
-            alt="menu"
-          />
-        </div>
-        <div className="text-3xl font-bold underline ml-8 mt-4">
-          <img
-            src="https://www.vmcdn.ca/f/files/via/images/pourhouse_burger_2_photochrisgiannakos.jpg;w=960"
-            alt="menu"
-          />
-        </div> */}
       </div>
     </>
   );
