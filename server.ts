9a7429aee26820407c0db22ece2e91dc1cf7d4af
OWNER <owner@KMBA.local>
2022-02-28 16:06:07 -0800

    connect mongodb atlas

--- server.ts
+++ server.ts
@@ -1,21 +1,16 @@
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const dbUrl =
+  "mongodb+srv://aujourdui:b50HX3xdE9QF6x4V@cluster0.lfbkc.mongodb.net/burger-data"; // dbの名前をcrudに指定
 
 const app = express();
-const port = process.env.PORT || 5000;
-dotenv.config({ path: "./config.env" });
+const port = 3001;
 
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-
-// import dbo from "./db/conn";
-const dbo = require("./db/conn");
-
-app.listen(port, () => {
-  dbo.connectToServer(function (err: any) {
-    if (err) console.error(err);
+mongoose.connect(dbUrl, (dbErr: any) => {
+  if (dbErr) throw new Error(dbErr);
+  else console.log("db connected");
+  app.listen(port, function () {
+    console.log("Server is running on port 3001");
   });
-  console.log(`listening on port ${port}`);
 });
